c7a64bf5fab68ca5dc059c3fd17a975aa78bc7b1
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-03-27 11:27:46 +0200

    created function to calculate final price

--- assignment3.py
+++ assignment3.py
@@ -1,4 +1,12 @@
 original_price  = 100
 discount_percentage = 25
 
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discounted_price = price - (price * discount_percent / 100)
+        return discounted_price
+    else:
+        return price
     
+final_price = calculate_discount(original_price, discount_percentage)
+print("Final price after discount", final_price)
